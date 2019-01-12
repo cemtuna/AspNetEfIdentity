504183f31371107e5dc544113ad6a4fcbdf77400
cemtuna <cemtuna@gmail.com>
2019-01-12 22:52:02 +0300

    Add project files.

---
+++ IdentityWebUI/Models/AccountDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace IdentityWebUI.Models
+{
+    public class AccountDbContext:IdentityDbContext<DssUser>
+    {
+        public AccountDbContext() : base()
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("DSS");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
